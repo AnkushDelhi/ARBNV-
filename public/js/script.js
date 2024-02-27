7e86dfee5c073e96bb0844ffdb411420a4973852
AnkushDelhi <huddaankush8@gmail.com>
2024-02-27 17:31:05 +0530

    add

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,7 @@
 
   let taxSwitch=document.getElementById("flexSwitchCheckDefault");
   taxSwitch.addEventListener("click",()=>{
-   let taxInfo=document.getElementsByClassName("tax");
+   let taxInfo=document.getElementsByClassName("tax-info");
     for (info of taxInfo){
       if(info.style.display != "inline"){
       info.style.display="inline";
